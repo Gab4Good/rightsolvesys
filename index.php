7fabc3c09268cce507f90bc848508880e8ebb170
Gabriel <njokutgabriel@gmail.com>
2025-08-07 18:41:00 +0000

    Further optimized load time

--- index.php
+++ index.php
@@ -4,54 +4,57 @@ ini_set("include_path", '/home/ctliehfj/php:' . ini_get("include_path"));
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
-
 function filter_string_polyfill(string $string): string
 {
     $str = preg_replace('/\x00|<[^>]*>?/', '', $string);
     return str_replace(["'", '"'], ['&#39;', '&#34;'], $str);
 }
 
-$mail = new PHPMailer; 
-//$mail->SMTPDebug = 3;      // Enable verbose debug output
-$mail->isSMTP();     // Set mailer to use SMTP
-$mail->Host = 'smtp.zoho.com;lim108.truehost.cloud';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;   // Enable SMTP authentication
-$mail->Username = 'engage@rightsolvesys.com';     // SMTP username
-$mail->Password = '';              // SMTP password
-$mail->SMTPSecure = 'tls';        // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;      // TCP port to connect to or 25 for non secure
-$mail->setFrom('engage@rightsolvesys.com', 'rightsolvesys.com');
-$mail->addAddress('engage@rightsolvesys.com', 'RightSolve Help Desk');     // Add a recipient
-$mail->addReplyTo('engage@rightsolvesys.com', 'RightSolve Help Desk');
-$mail->isHTML(false);            // Set email format to HTML
-$mail->Subject = 'Message from website visitor';
-
-if (isset($_POST["inputEmail"])) {
+// Only process form data if it's a POST request
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["inputEmail"])) {
     
-  if ($_POST["inputEmail"] != '' && $_POST["firstName"] != '' && $_POST["lastName"] != '' && $_POST["phone"] != '' && $_POST["msgTextarea"] != '') {
-		
-	$SenderEmail= filter_input(INPUT_POST, "inputEmail", FILTER_SANITIZE_EMAIL);
-	$firstName= filter_string_polyfill($_POST["firstName"]);
-	$lastName= filter_string_polyfill($_POST["lastName"]);
-	$phone= filter_string_polyfill($_POST["phone"]);
-    $message= filter_string_polyfill($_POST["msgTextarea"]);
-    $mailBody="Name: $firstName . '' . $lastName\r\nEmail: $SenderEmail\r\Phone: $phone\r\nVisitor Message: $message";
-    
-	$mail->Body    = $mailBody;
-	if(!$mail->send()) {
-		echo '<script>alert("Sorry! We could not send your message at this time. Please try again later.")</script>';
-		$mail->ErrorInfo;
-	} else {
-		echo '<script>alert("Thank you! Your message has been sent.\r\n A member of the team will respond soon.")</script>';
-	}
+    // Require PHPMailer files only when needed
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
 
-} else {
-echo '<script>alert("Please review your entries and try again. All fields are required!")</script>';
-}
+    // Use !empty() for a more robust check
+    if (!empty($_POST["inputEmail"]) && !empty($_POST["firstName"]) && !empty($_POST["lastName"]) && !empty($_POST["phone"]) && !empty($_POST["msgTextarea"])) {
+        
+        $mail = new PHPMailer; 
+        //$mail->SMTPDebug = 3;      // Enable verbose debug output
+        $mail->isSMTP();     // Set mailer to use SMTP
+        $mail->Host = 'smtp.zoho.com;lim108.truehost.cloud';  // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;   // Enable SMTP authentication
+        $mail->Username = 'engage@rightsolvesys.com';     // SMTP username
+        $mail->Password = '';              // SMTP password
+        $mail->SMTPSecure = 'tls';        // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;      // TCP port to connect to or 25 for non secure
+        $mail->setFrom('engage@rightsolvesys.com', 'rightsolvesys.com');
+        $mail->addAddress('engage@rightsolvesys.com', 'RightSolve Help Desk');     // Add a recipient
+        $mail->addReplyTo('engage@rightsolvesys.com', 'RightSolve Help Desk');
+        $mail->isHTML(false);            // Set email format to HTML
+        $mail->Subject = 'Message from website visitor';
 
+        $SenderEmail= filter_input(INPUT_POST, "inputEmail", FILTER_SANITIZE_EMAIL);
+        $firstName= filter_string_polyfill($_POST["firstName"]);
+        $lastName= filter_string_polyfill($_POST["lastName"]);
+        $phone= filter_string_polyfill($_POST["phone"]);
+        $message= filter_string_polyfill($_POST["msgTextarea"]);
+        $mailBody="Name: {$firstName} {$lastName}\r\nEmail: {$SenderEmail}\r\nPhone: {$phone}\r\nVisitor Message: {$message}";
+        
+        $mail->Body    = $mailBody;
+        if(!$mail->send()) {
+            echo '<script>alert("Sorry! We could not send your message at this time. Please try again later.")</script>';
+            // The following line doesn't do anything. You might want to log the error.
+            // error_log("Mailer Error: " . $mail->ErrorInfo);
+        } else {
+            echo '<script>alert("Thank you! Your message has been sent.\r\n A member of the team will respond soon.")</script>';
+        }
+
+    } else {
+        echo '<script>alert("Please review your entries and try again. All fields are required!")</script>';
+    }
 }
 
 ?>
@@ -138,6 +141,33 @@ window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/14
 <link property="stylesheet" rel='stylesheet' id='trx_demo_icons-css' href='wp-content/plugins/trx_demo/css/font-icons/css/trx_demo_icons.css' type='text/css' media='all' />
 <link property="stylesheet" rel='stylesheet' id='n7-golf-club-fontello-css' href='wp-content/themes/n7-golf-club/skins/default/css/font-icons/css/fontello.css' type='text/css' media='all' />
 <link property="stylesheet" rel='stylesheet' id='sbi_styles-css' href='wp-content/plugins/instagram-feed/css/sbi-styles.minfc7a.css?ver=6.0.6' type='text/css' media='all' />
+<style id='wp-emoji-styles-inline-css' type='text/css'>
+
+	img.wp-smiley, img.emoji {
+		display: inline !important;
+		border: none !important;
+		box-shadow: none !important;
+		height: 1em !important;
+		width: 1em !important;
+		margin: 0 0.07em !important;
+		vertical-align: -0.1em !important;
+		background: none !important;
+		padding: 0 !important;
+	}
+</style>
+<link property="stylesheet" rel='stylesheet' id='wp-block-library-css' href='wp-includes/css/dist/block-library/style.min84fc.css?ver=6.4.3' type='text/css' media='all' />
+
+<style id='classic-theme-styles-inline-css' type='text/css'>
+/*! This file is auto-generated */
+.wp-block-button__link{color:#fff;background-color:#32373c;border-radius:9999px;box-shadow:none;text-decoration:none;padding:calc(.667em + 2px) calc(1.333em + 2px);font-size:1.125em}.wp-block-file__button{background:#32373c;color:#fff;text-decoration:none}
+</style>
+<style id='global-styles-inline-css' type='text/css'>
+body{--wp--preset--color--black: #000000;--wp--preset--color--cyan-bluish-gray: #abb8c3;--wp--preset--color--white: #ffffff;--wp--preset--color--pale-pink: #f78da7;--wp--preset--color--vivid-red: #cf2e2e;--wp--preset--color--luminous-vivid-orange: #ff6900;--wp--preset--color--luminous-vivid-amber: #fcb900;--wp--preset--color--light-green-cyan: #7bdcb5;--wp--preset--color--vivid-green-cyan: #00d084;--wp--preset--color--pale-cyan-blue: #8ed1fc;--wp--preset--color--vivid-cyan-blue: #0693e3;--wp--preset--color--vivid-purple: #9b51e0;--wp--preset--color--bg-color: #FFFFFF;--wp--preset--color--bd-color: #DBD9D7;--wp--preset--color--text-dark: #1D1A0C;--wp--preset--color--text-light: #9A9694;--wp--preset--color--text-link: #07d368;--wp--preset--color--text-hover: #1BA25B;--wp--preset--color--text-link-2: #A6985B;--wp--preset--color--text-hover-2: #908246;--wp--preset--color--text-link-3: #9AA65B;--wp--preset--color--text-hover-3: #818D44;--wp--preset--gradient--vivid-cyan-blue-to-vivid-purple: linear-gradient(135deg,rgba(6,147,227,1) 0%,rgb(155,81,224) 100%);--wp--preset--gradient--light-green-cyan-to-vivid-green-cyan: linear-gradient(135deg,rgb(122,220,180) 0%,rgb(0,208,130) 100%);--wp--preset--gradient--luminous-vivid-amber-to-luminous-vivid-orange: linear-gradient(135deg,rgba(252,185,0,1) 0%,rgba(255,105,0,1) 100%);--wp--preset--gradient--luminous-vivid-orange-to-vivid-red: linear-gradient(135deg,rgba(255,105,0,1) 0%,rgb(207,46,46) 100%);--wp--preset--gradient--very-light-gray-to-cyan-bluish-gray: linear-gradient(135deg,rgb(238,238,238) 0%,rgb(169,184,195) 100%);--wp--preset--gradient--cool-to-warm-spectrum: linear-gradient(135deg,rgb(74,234,220) 0%,rgb(151,120,209) 20%,rgb(207,42,186) 40%,rgb(238,44,130) 60%,rgb(251,105,98) 80%,rgb(254,248,76) 100%);--wp--preset--gradient--blush-light-purple: linear-gradient(135deg,rgb(255,206,236) 0%,rgb(152,150,240) 100%);--wp--preset--gradient--blush-bordeaux: linear-gradient(135deg,rgb(254,205,165) 0%,rgb(254,45,45) 50%,rgb(107,0,62) 100%);--wp--preset--gradient--luminous-dusk: linear-gradient(135deg,rgb(255,203,112) 0%,rgb(199,81,192) 50%,rgb(65,88,208) 100%);--wp--preset--gradient--pale-ocean: linear-gradient(135deg,rgb(255,245,203) 0%,rgb(182,227,212) 50%,rgb(51,167,181) 100%);--wp--preset--gradient--electric-grass: linear-gradient(135deg,rgb(202,248,128) 0%,rgb(113,206,126) 100%);--wp--preset--gradient--midnight: linear-gradient(135deg,rgb(2,3,129) 0%,rgb(40,116,252) 100%);--wp--preset--font-size--small: 13px;--wp--preset--font-size--medium: 20px;--wp--preset--font-size--large: 36px;--wp--preset--font-size--x-large: 42px;--wp--preset--spacing--20: 0.44rem;--wp--preset--spacing--30: 0.67rem;--wp--preset--spacing--40: 1rem;--wp--preset--spacing--50: 1.5rem;--wp--preset--spacing--60: 2.25rem;--wp--preset--spacing--70: 3.38rem;--wp--preset--spacing--80: 5.06rem;--wp--preset--shadow--natural: 6px 6px 9px rgba(0, 0, 0, 0.2);--wp--preset--shadow--deep: 12px 12px 50px rgba(0, 0, 0, 0.4);--wp--preset--shadow--sharp: 6px 6px 0px rgba(0, 0, 0, 0.2);--wp--preset--shadow--outlined: 6px 6px 0px -3px rgba(255, 255, 255, 1), 6px 6px rgba(0, 0, 0, 1);--wp--preset--shadow--crisp: 6px 6px 0px rgba(0, 0, 0, 1);}:where(.is-layout-flex){gap: 0.5em;}:where(.is-layout-grid){gap: 0.5em;}body .is-layout-flow > .alignleft{float: left;margin-inline-start: 0;margin-inline-end: 2em;}body .is-layout-flow > .alignright{float: right;margin-inline-start: 2em;margin-inline-end: 0;}body .is-layout-flow > .aligncenter{margin-left: auto !important;margin-right: auto !important;}body .is-layout-constrained > .alignleft{float: left;margin-inline-start: 0;margin-inline-end: 2em;}body .is-layout-constrained > .alignright{float: right;margin-inline-start: 2em;margin-inline-end: 0;}body .is-layout-constrained > .aligncenter{margin-left: auto !important;margin-right: auto !important;}body .is-layout-constrained > :where(:not(.alignleft):not(.alignright):not(.alignfull)){max-width: var(--wp--style--global--content-size);margin-left: auto !important;margin-right: auto !important;}body .is-layout-constrained > .alignwide{max-width: var(--wp--style--global--wide-size);}body .is-layout-flex{display: flex;}body .is-layout-flex{flex-wrap: wrap;align-items: center;}body .is-layout-flex > *{margin: 0;}body .is-layout-grid{display: grid;}body .is-layout-grid > *{margin: 0;}:where(.wp-block-columns.is-layout-flex){gap: 2em;}:where(.wp-block-columns.is-layout-grid){gap: 2em;}:where(.wp-block-post-template.is-layout-flex){gap: 1.25em;}:where(.wp-block-post-template.is-layout-grid){gap: 1.25em;}.has-black-color{color: var(--wp--preset--color--black) !important;}.has-cyan-bluish-gray-color{color: var(--wp--preset--color--cyan-bluish-gray) !important;}.has-white-color{color: var(--wp--preset--color--white) !important;}.has-pale-pink-color{color: var(--wp--preset--color--pale-pink) !important;}.has-vivid-red-color{color: var(--wp--preset--color--vivid-red) !important;}.has-luminous-vivid-orange-color{color: var(--wp--preset--color--luminous-vivid-orange) !important;}.has-luminous-vivid-amber-color{color: var(--wp--preset--color--luminous-vivid-amber) !important;}.has-light-green-cyan-color{color: var(--wp--preset--color--light-green-cyan) !important;}.has-vivid-green-cyan-color{color: var(--wp--preset--color--vivid-green-cyan) !important;}.has-pale-cyan-blue-color{color: var(--wp--preset--color--pale-cyan-blue) !important;}.has-vivid-cyan-blue-color{color: var(--wp--preset--color--vivid-cyan-blue) !important;}.has-vivid-purple-color{color: var(--wp--preset--color--vivid-purple) !important;}.has-black-background-color{background-color: var(--wp--preset--color--black) !important;}.has-cyan-bluish-gray-background-color{background-color: var(--wp--preset--color--cyan-bluish-gray) !important;}.has-white-background-color{background-color: var(--wp--preset--color--white) !important;}.has-pale-pink-background-color{background-color: var(--wp--preset--color--pale-pink) !important;}.has-vivid-red-background-color{background-color: var(--wp--preset--color--vivid-red) !important;}.has-luminous-vivid-orange-background-color{background-color: var(--wp--preset--color--luminous-vivid-orange) !important;}.has-luminous-vivid-amber-background-color{background-color: var(--wp--preset--color--luminous-vivid-amber) !important;}.has-light-green-cyan-background-color{background-color: var(--wp--preset--color--light-green-cyan) !important;}.has-vivid-green-cyan-background-color{background-color: var(--wp--preset--color--vivid-green-cyan) !important;}.has-pale-cyan-blue-background-color{background-color: var(--wp--preset--color--pale-cyan-blue) !important;}.has-vivid-cyan-blue-background-color{background-color: var(--wp--preset--color--vivid-cyan-blue) !important;}.has-vivid-purple-background-color{background-color: var(--wp--preset--color--vivid-purple) !important;}.has-black-border-color{border-color: var(--wp--preset--color--black) !important;}.has-cyan-bluish-gray-border-color{border-color: var(--wp--preset--color--cyan-bluish-gray) !important;}.has-white-border-color{border-color: var(--wp--preset--color--white) !important;}.has-pale-pink-border-color{border-color: var(--wp--preset--color--pale-pink) !important;}.has-vivid-red-border-color{border-color: var(--wp--preset--color--vivid-red) !important;}.has-luminous-vivid-orange-border-color{border-color: var(--wp--preset--color--luminous-vivid-orange) !important;}.has-luminous-vivid-amber-border-color{border-color: var(--wp--preset--color--luminous-vivid-amber) !important;}.has-light-green-cyan-border-color{border-color: var(--wp--preset--color--light-green-cyan) !important;}.has-vivid-green-cyan-border-color{border-color: var(--wp--preset--color--vivid-green-cyan) !important;}.has-pale-cyan-blue-border-color{border-color: var(--wp--preset--color--pale-cyan-blue) !important;}.has-vivid-cyan-blue-border-color{border-color: var(--wp--preset--color--vivid-cyan-blue) !important;}.has-vivid-purple-border-color{border-color: var(--wp--preset--color--vivid-purple) !important;}.has-vivid-cyan-blue-to-vivid-purple-gradient-background{background: var(--wp--preset--gradient--vivid-cyan-blue-to-vivid-purple) !important;}.has-light-green-cyan-to-vivid-green-cyan-gradient-background{background: var(--wp--preset--gradient--light-green-cyan-to-vivid-green-cyan) !important;}.has-luminous-vivid-amber-to-luminous-vivid-orange-gradient-background{background: var(--wp--preset--gradient--luminous-vivid-amber-to-luminous-vivid-orange) !important;}.has-luminous-vivid-orange-to-vivid-red-gradient-background{background: var(--wp--preset--gradient--luminous-vivid-orange-to-vivid-red) !important;}.has-very-light-gray-to-cyan-bluish-gray-gradient-background{background: var(--wp--preset--gradient--very-light-gray-to-cyan-bluish-gray) !important;}.has-cool-to-warm-spectrum-gradient-background{background: var(--wp--preset--gradient--cool-to-warm-spectrum) !important;}.has-blush-light-purple-gradient-background{background: var(--wp--preset--gradient--blush-light-purple) !important;}.has-blush-bordeaux-gradient-background{background: var(--wp--preset--gradient--blush-bordeaux) !important;}.has-luminous-dusk-gradient-background{background: var(--wp--preset--gradient--luminous-dusk) !important;}.has-pale-ocean-gradient-background{background: var(--wp--preset--gradient--pale-ocean) !important;}.has-electric-grass-gradient-background{background: var(--wp--preset--gradient--electric-grass) !important;}.has-midnight-gradient-background{background: var(--wp--preset--gradient--midnight) !important;}.has-small-font-size{font-size: var(--wp--preset--font-size--small) !important;}.has-medium-font-size{font-size: var(--wp--preset--font-size--medium) !important;}.has-large-font-size{font-size: var(--wp--preset--font-size--large) !important;}.has-x-large-font-size{font-size: var(--wp--preset--font-size--x-large) !important;}
+.wp-block-navigation a:where(:not(.wp-element-button)){color: inherit;}
+:where(.wp-block-post-template.is-layout-flex){gap: 1.25em;}:where(.wp-block-post-template.is-layout-grid){gap: 1.25em;}
+:where(.wp-block-columns.is-layout-flex){gap: 2em;}:where(.wp-block-columns.is-layout-grid){gap: 2em;}
+.wp-block-pullquote{font-size: 1.5em;line-height: 1.6;}
+</style>
 <link property="stylesheet" rel='stylesheet' id='booked-tooltipster-css' href='wp-content/plugins/quickcal/assets/js/tooltipster/css/tooltipster9b70.css?ver=3.3.0' type='text/css' media='all' />
 <link property="stylesheet" rel='stylesheet' id='booked-tooltipster-theme-css' href='wp-content/plugins/quickcal/assets/js/tooltipster/css/themes/tooltipster-light9b70.css?ver=3.3.0' type='text/css' media='all' />
 <link property="stylesheet" rel='stylesheet' id='trx_demo_panels-css' href='wp-content/plugins/trx_demo/css/trx_demo_panels.css' type='text/css' media='all' />
